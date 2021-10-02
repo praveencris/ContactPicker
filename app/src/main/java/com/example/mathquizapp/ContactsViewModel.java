c3e963648ceb2f5d9df15f2c2cb1c727f90deb1b
Praveen <pkchmits@gmail.com>
2021-10-03 00:16:09 +0530

    Done with contact loading inside activity

---
+++ app/src/main/java/com/example/mathquizapp/ContactsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.mathquizapp;
+
+import androidx.lifecycle.ViewModel;
+
+public class ContactsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/mathquizapp/MainActivity.java
+++ app/src/main/java/com/example/mathquizapp/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.mathquizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.cursoradapter.widget.SimpleCursorAdapter;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 
 import com.example.mathquizapp.databinding.ActivityMainBinding;
@@ -10,18 +19,84 @@ import com.example.mathquizapp.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Cursor cursor;
+    SimpleCursorAdapter  adapter;
+    // Defines the id of the loader for later reference
+    public static final int CONTACT_LOADER_ID = 78;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS}, PackageManager.PERMISSION_GRANTED);
+
         setContentView(binding.getRoot());
 
+        //Bind adapter to list
+        setupCursorAdapter();
+        // Initialize the loader with a special ID and the defined callbacks from above
+        getSupportLoaderManager().initLoader(CONTACT_LOADER_ID,
+                new Bundle(), contactsLoader);
 
         binding.getContactBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+             binding.listView.setAdapter(adapter);
             }
         });
     }
+
+    private void setupCursorAdapter() {
+        // Column data from cursor to bind views from
+        String[] uiBindFrom = { ContactsContract.Contacts.DISPLAY_NAME };
+        // View IDs which will have the respective column data inserted
+        int[] uiBindTo = { android.R.id.text1 };
+        // Create the simple cursor adapter to use for our list
+        // specifying the template to inflate (item_contact),
+        adapter = new SimpleCursorAdapter(
+                this, R.layout.contacts_list_item,
+                null, uiBindFrom, uiBindTo,
+                0);
+    }
+
+
+    // Defines the asynchronous callback for the contacts data loader
+    private LoaderManager.LoaderCallbacks<Cursor> contactsLoader =
+            new LoaderManager.LoaderCallbacks<Cursor>() {
+                // Create and return the actual cursor loader for the contacts data
+                @Override
+                public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                    // Define the columns to retrieve
+                    String[] projectionFields = new String[] { ContactsContract.Contacts._ID,
+                            ContactsContract.Contacts.DISPLAY_NAME};
+                    // Construct the loader
+                    CursorLoader cursorLoader = new CursorLoader(MainActivity.this,
+                            ContactsContract.Contacts.CONTENT_URI, // URI
+                            projectionFields, // projection fields
+                            null, // the selection criteria
+                            null, // the selection args
+                            null // the sort order
+                    );
+                    // Return the loader for use
+                    return cursorLoader;
+                }
+
+                // When the system finishes retrieving the Cursor through the CursorLoader,
+                // a call to the onLoadFinished() method takes place.
+                @Override
+                public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+                    // The swapCursor() method assigns the new Cursor to the adapter
+                    adapter.swapCursor(cursor);
+                }
+
+                // This method is triggered when the loader is being reset
+                // and the loader data is no longer available. Called if the data
+                // in the provider changes and the Cursor becomes stale.
+                @Override
+                public void onLoaderReset(Loader<Cursor> loader) {
+                    // Clear the Cursor we were using with another call to the swapCursor()
+                    adapter.swapCursor(null);
+                }
+            };
+
+
 }
