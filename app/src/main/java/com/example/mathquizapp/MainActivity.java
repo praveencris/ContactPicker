688a21ca0305ab1833ccca1ee5256117bc0781b5
Praveen <pkchmits@gmail.com>
2021-10-03 10:51:56 +0530

    Feature to read contacts and setting up dynamic permissions done

--- app/src/main/java/com/example/mathquizapp/MainActivity.java
+++ app/src/main/java/com/example/mathquizapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mathquizapp;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -8,32 +9,67 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.mathquizapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int RC_PIC_CONTACT = 100;
+    private static final int RC_READ_CONTACTS = 200;
     private ActivityMainBinding binding;
+    private SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        sessionManager = new SessionManager(this);
 
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_CONTACTS},
+                RC_READ_CONTACTS);
 
         binding.getContactBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
-                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
-                startActivityForResult(contactPickerIntent, RC_PIC_CONTACT);
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    requestContact();
+                } else {
+                    doIfNotGranted();
+                }
             }
         });
+    }
+
+    private void requestContact() {
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, RC_PIC_CONTACT);
+    }
+
+    private void doIfNotGranted() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                Manifest.permission.READ_CONTACTS)) {
+            // Show an explanation to the user
+            Toast.makeText(MainActivity.this, "Permission is needed to read contacts!", Toast.LENGTH_LONG).show();
+        }
+        if (sessionManager.isFirstTimeAsking(Manifest.permission.READ_CONTACTS)) {
+            sessionManager.firstTimeAsking(Manifest.permission.READ_CONTACTS, false);
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    RC_READ_CONTACTS);
+        } else {
+            //Permission disable by device policy or user denied permanently. Show proper error message
+            Toast.makeText(MainActivity.this, "Permission is needed to read contacts, to avail this feature give permission from app setting.", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -59,6 +95,26 @@ public class MainActivity extends AppCompatActivity {
             }
         } else {
             Log.e("MainActivity", "Failed to pick contact");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case RC_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Permission was granted. Now you can call your method to open contacts, fetch contact or whatever
+                   requestContact();
+                } else {
+                    // Permission was denied.......
+                    // You can again ask for permission from here
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+
         }
     }
 
